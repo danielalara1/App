6b8fa7e043f09e627cb7be4e187774b61e404e73
Daniela Lara <laradaniela9666@gmail.com>
2026-05-14 17:37:46 +0200

    paso 14

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { SubirVibe } from "./pages/SubirVibe"; 
+import Profile from "./pages/profile"; 
+import NotFound from "./pages/NotFound"; 
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Home />} />
-        
-        <Route path="/subir" element={<SubirVibe />} />
+       <Route path="/subir" element={<SubirVibe />} />
+
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
